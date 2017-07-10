d79dcc36b72ce6e84d45cec14ed203cf84c128b2
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-07-10 10:21:01 +0200

    added string sorting function

--- src/com/greenfox/Main.java
+++ src/com/greenfox/Main.java
@@ -4,25 +4,45 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        String[] a = {"one", "two", "three"};
-        String[] b = {"two", "three", "one"};
+    String[] a = {"one", "two", "three"};
+    String[] b = {"two", "three", "one"};
 
-        System.out.println(arrayMatcher(a, b));
+    System.out.println(arrayMatcher(a, b));
 
-    }
+  }
 
-    public static boolean arrayMatcher( String[] x, String[] y) {
-      if (x.length != y.length)
+  public static boolean arrayMatcher(String[] x, String[] y) {
+    if (x.length != y.length)
+      return false;
+    int arrayLength = x.length;
+    sortArray(x);
+    sortArray(y);
+    for (int i = 0; i < arrayLength; i++) {
+      if (x[i] != y[i])
         return false;
-      int arrayLength = x.length;
-      Arrays.sort(x);
-      Arrays.sort(y);
-      for (int i = 0; i < arrayLength; i++) {
-        if (x[i] != y[i])
-          return false;
-      }
-      return true;
     }
+    return true;
+  }
+
+  public static void sortArray(String[] array) {
+    for (int i = 0; i < array.length; i++) {
+      for (int j = 0; j < i; j++) {
+        int n = 0;
+        if (array[i].toLowerCase().charAt(n) == array[j].toLowerCase().charAt(n)) {
+          n++;
+        }
+        else if (array[i].toLowerCase().charAt(n) > array[j].toLowerCase().charAt(n)) {
+          swap(array, i, j);
+        }
+      }
+    }
+  }
+
+  public static void swap(String[] x, int i, int j){
+    String temp = x[i];
+    x[i] = x[j];
+    x[j] = temp;
+  }
 }
